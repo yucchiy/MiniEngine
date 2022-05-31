19d03700e6a2774e3bf61f2a945f469f8619bdbb
Yuichiro Mukai <y.iky917@gmail.com>
2022-06-01 06:10:13 +0900

    Setup typescript & webpack environment

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # MiniEngine
+
+My private project for studying 3DCG.
+
+## Requirements
+
+MiniEngine requires Node.js. We are testing it on v16.4.0.
+
+## Usage
+
+```
+$ npm install
+```
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+ var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var debug = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/entrypoint.ts'),
+    plugins: [
+        new HtmlWebpackPlugin({
+            templateParameters: {
+                title: 'MiniEngein',
+            },
+            template: path.resolve(__dirname, 'src/index.html'),
+            inject: 'body',
+        })
+    ],
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(frag|vert|glsl)$/,
+                include: [
+                    path.resolve(__dirname, 'src/chibi/glsl'),
+                ],
+                use: [
+                    'raw-loader'
+                ]
+            },
+            {
+                test: /\.(tsx|ts)$/,
+                exclude: /node_modules/,
+                use: [
+                    'ts-loader'
+                ]
+            }
+        ]
+    },
+    devServer: {
+        hot: true,
+    },
+    mode: debug ? 'development' : 'production',
+    devtool: debug ? 'eval-source-map' : 'source-map',
+}; 
